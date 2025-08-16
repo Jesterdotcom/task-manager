2c541811935c88ce606f272d9802c28867f0823e
Prem Singh <itzpremsingh@duck.com>
2025-08-16 14:29:03 +0530

    Create utility.py

---
+++ utility.py
@@ -0,0 +1,7 @@
+def display() - None:
+    print("Welcome To Task Manager")
+    print()
+    print("1. Add task")
+    print("2. Update task")
+    print("3. Delete task")
+    print("4. Mark completed") 
