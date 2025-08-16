56e963b6c5b477f1c887cad4c62db37ab5e82552
Jay Kumar <jayk13679@gmail.com>
2025-08-16 15:30:31 +0530

    Update main.py
    Added input feature

--- main.py
+++ main.py
@@ -4,4 +4,15 @@ print("Welcome User")
 print("This Program is to help you manage your daily tasks")
 print(option())
 n = int(input("Enter 1/2/3/4 according to their corresponding functions: "))
+if n == 1 :
+    '''add function 1 here'''
+elif n == 2 :
+       '''add function 2 here'''
+elif n == 3 :
+       '''add function 3 here'''
+elif n == 4 :
+       '''add function 4 here'''
+else :
+      print("Please enter right option number")       
+
 
