a7eeace70543a6cc0919bf33554e70ed53e591a7
Prem Singh <itzpremsingh@duck.com>
2025-08-16 16:39:29 +0530

    Update main.py
    Formatted and called the add function.

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 #HOME PAGE
-from utility import option
+from utility import option, Manager
+
+manager = Manager("database.db") 
+
 print("Welcome User")
 print("This Program is to help you manage your daily tasks")
+
 print(option())
 n = int(input("Enter 1/2/3/4 according to their corresponding functions: "))
+
 if n == 1 :
-    '''add function 1 here'''
+    task = input("Enter task name:")
+    manager.add(task)
+    
 elif n == 2 :
        '''add function 2 here'''
+    
 elif n == 3 :
        '''add function 3 here'''
+    
 elif n == 4 :
        '''add function 4 here'''
+    
 else :
       print("Please enter right option number")       
-
-
