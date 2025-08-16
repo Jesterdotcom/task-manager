8f53f0b1bc8c148930e6f126dd1aa47609ff3f19
Prem Singh <itzpremsingh@duck.com>
2025-08-16 18:03:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
-#HOME PAGE
-from utility import option, Manager
+import functions
+from classes import Manager
 
-manager = Manager("database.db") 
+manager = Manager("database.db")
 
-print("Welcome User")
-print("This Program is to help you manage your daily tasks")
+print("Welcome to your task manager")
 
-print(option())
-n = int(input("Enter 1/2/3/4 according to their corresponding functions: "))
+while True:
+    functions.option()
+    try:
+        n = int(input("Enter your option: "))
+    except ValueError:
+        print("Invalid option")
+        continue
 
-if n == 1 :
-    task = input("Enter task name:")
-    manager.add(task)
-    
-elif n == 2 :
-       '''add function 2 here'''
-    
-elif n == 3 :
-       '''add function 3 here'''
-    
-elif n == 4 :
-       '''add function 4 here'''
-    
-else :
-      print("Please enter right option number")       
+    if n == 1:
+        functions.list(manager)
+
+    elif n == 2:
+        functions.add(manager)
+
+    elif n == 3:
+        functions.update(manager)
+
+    elif n == 4:
+        functions.delete(manager)
+
+    elif n == 5:
+        functions.mark(manager)
+
+    elif n == 6:
+        print("Goodbye")
+        break
+
+    else:
+        print("Invalid option")
