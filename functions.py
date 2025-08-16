9e4b968e561e1c491a769d41aaff3ccde07632c4
Prem Singh <itzpremsingh@duck.com>
2025-08-16 18:05:00 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,59 @@
+from classes import Manager
+
+
+def option() -> None:
+    options = ("List tasks", "Add task", "Update task", "Delete task", "Mark completed")
+
+    print()
+    for index, opt in enumerate(options):
+        print(f"{index + 1}. {opt}")
+
+    print()
+
+
+def list(manager: Manager) -> None:
+    pend = manager.list().pending()
+    if not pend:
+        print("No pending tasks")
+        return
+
+    for id, name in pend:
+        print("List of pending tasks:")
+        print(f"{id}. {name}")
+
+
+def add(manager: Manager) -> None:
+    task = input("Enter task name: ")
+    manager.add(task)
+    print("Task added successfully")
+
+
+def update(manager: Manager) -> None:
+    id = int(input("Enter task id: "))
+    if not manager.exists(id):
+        print("Task does not exist")
+        return
+
+    task = input("Enter task name: ")
+    manager.update(id).name(task)
+    print("Task updated successfully")
+
+
+def delete(manager: Manager) -> None:
+    id = int(input("Enter task id: "))
+    if not manager.exists(id):
+        print("Task does not exist")
+        return
+
+    manager.delete(id)
+    print("Task deleted successfully")
+
+
+def mark(manager: Manager) -> None:
+    id = int(input("Enter task id: "))
+    if not manager.exists(id):
+        print("Task does not exist")
+        return
+
+    manager.update(id).complete()
+    print("Task marked as completed")
